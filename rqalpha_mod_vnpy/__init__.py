3d05309736f80bbb9b5f1202e66d6ffaba44acf6
EricWang <wangh@ricequant.com>
2017-03-29 14:35:56 +0800

    update license

--- rqalpha_mod_vnpy/__init__.py
+++ rqalpha_mod_vnpy/__init__.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from .mod import VNPYMod
-
-
-def load_mod():
-    return VNPYMod()
+#
+# Copyright 2017 Ricequant, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 __config__ = {
@@ -19,3 +29,8 @@ __config__ = {
         'mdAddress': 'tcp://180.168.146.187:10031'
     }
 }
+
+
+def load_mod():
+    from .mod import VNPYMod
+    return VNPYMod()
--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Ricequant, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os.path import dirname, join
 from pip.req import parse_requirements
 
 from setuptools import (
@@ -5,13 +23,17 @@ from setuptools import (
     setup,
 )
 
+
+with open(join(dirname(__file__), 'rqalpha_mod_vnpy/VERSION.txt'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
+
 setup(
     name='rqalpha-mod-vnpy',
-    version="0.9.7",
-    description='A mod for RQAlpha to connect weith VN.PY',
+    version=version,
+    description='A mod for RQAlpha to connect with VN.PY',
     packages=find_packages(exclude=[]),
-    author='Cuizi7',
-    author_email='cuizi7@163.com',
+    author='ricequant',
+    author_email='public@ricequant.com',
     license='Apache License v2',
     package_data={'': ['*.*']},
     url='https://github.com/ricequant/rqalpha-mod-vnpy',
